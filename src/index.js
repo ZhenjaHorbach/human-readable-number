840371cc256903714f39c15eec631cf1cab43f0c
ZhenjaHorbach <HorbachZhenja@yandex.by>
2020-10-04 17:23:55 +0300

    feat:all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+	let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+	let numberTy = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+	let numberTeen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+	let result = [];
+	if (number == 0) {
+		return 'zero';
+	}
+	if (number / 100 >= 1) {
+		result.push(`${numbers[Math.floor(number / 100 - 1)]} hundred`)
+	}
+	if (number % 100 > 0 && number % 100 < 100) {
+		if (number % 100 >= 10 && number % 100 <= 19) {
+			result.push(`${numberTeen[number % 10]}`)
+			return result.join(' ');
+		} else if (number % 100 / 10 >= 2 && number % 100 / 10 < 10) {
+			result.push(`${numberTy[Math.floor(number % 100 / 10 - 2)]}`)
+		}
+	}
+	if (number % 10 !== 0) {
+		result.push(`${numbers[number % 10 - 1]}`)
+	}
+
+
+
+	return result.join(' ');
 }
